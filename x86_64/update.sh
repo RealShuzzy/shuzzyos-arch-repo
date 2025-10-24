b12c724fbd2b983f9bd88f4b53240b00bc92f1f1
RealShuzzy <samirvonhansen2016@gmail.com>
2025-10-24 11:06:57 +0200

    added waybar

---
+++ x86_64/update.sh
@@ -0,0 +1,10 @@
+# Remove previous files
+rm shzzyos-arch-repo.{db,files}
+
+# Create new files
+repo-add shuzzyos-arch-repo.db.tar.gz *.pkg.tar.zst
+
+# Simplify
+rm shuzzyos-arch-repo.{db,files}
+mv shuzzyos-arch-repo.db.tar.gz shuzzyos-arch-repo.db
+mv shuzzyos-arch-repo.files.tar.gz shuzzyos-arch-repo.files
